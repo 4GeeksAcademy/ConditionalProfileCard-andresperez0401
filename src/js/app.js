12361b2bd48d44eb6f3acf952703683181d5f30e
Andres Perez <andresperez0401@gmail.com>
2025-01-28 12:48:59 -0400

    Perfil condicional Andres Perez Realizado exitosamente

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,42 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>
+            ${variables.name == null ? "Andrés" : variables.name}
+            ${variables.lastName == null ? "Pérez" : variables.lastName}
+          </h1>
+          
+          <h2>${
+            variables.role == null ? "Full-Stack Developer" : variables.role
+          }</h2>
+          <h3>${variables.city == null ? "Caracas, " : variables.city + ", "}
+              ${
+                variables.country == null
+                  ? "Venezuela."
+                  : variables.country + "."
+              }
+          </h3>
+          ${
+            variables.socialMediaPosition == "position-right"
+              ? `<ul class="position-right">`
+              : `<ul class="position-left">`
+          }
+           <li><a href="https://twitter.com/${
+             variables.twitter == null ? "4geeksacademy" : variables.twitter
+           }"><i class="fab fa-twitter"></i></a></li>
+
+           <li><a href="https://github.com/${
+             variables.github == null ? "4geeksacademy" : variables.github
+           }"><i class="fab fa-github"></i></a></li>
+
+           <li><a href="https://linkedin.com/${
+             variables.linkedin == null ? "4geeksacademy" : variables.linkedin
+           }"><i class="fab fa-linkedin"></i></a></li>
+
+           <li><a href="https://instagram.com/${
+             variables.instagram == null ? "4geeksacademy" : variables.instagram
+           }"><i class="fab fa-instagram"></i></a></li>
+    
           </ul>
         </div>
     `;
@@ -56,7 +84,7 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "./Andres.jpeg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
